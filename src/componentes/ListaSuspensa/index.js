22121bd3725fedc23fa73552ff14cd38b4e30bbd
Eugênio Francisco da Silva <eugeniofr17@gmail.com>
2023-05-04 12:14:00 -0300

    Criado todo o forms e parte das listas

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -2,9 +2,9 @@ import './ListaSuspensa.css'
 
 const ListaSuspensa = (props)=>{
     return(
-        <div>
+        <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select>
+            <select onChange={evento => props.aoAlterar(evento.target.value)} required={props.required} value={props.value}>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
