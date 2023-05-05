2e78b9f985867d66b2c0b8bc733845279c5fc503
Eugênio Francisco da Silva <eugeniofr17@gmail.com>
2023-05-05 16:42:25 -0300

    Finalizando cards e rodapé

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
+import Rodape from './componentes/Rodape';
 
 
 function App() {
@@ -60,9 +61,15 @@ function App() {
       <Banner/>
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
       
-      {times.map(time =>  <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
+      {times.map(time =>  <Time 
+      key={time.nome} 
+      nome={time.nome} 
+      corPrimaria={time.corPrimaria} 
+      corSecundaria={time.corSecundaria}
+      colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}
       
-      
+      <Rodape/>
     </div>
   );
 }
